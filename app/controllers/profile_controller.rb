35aa547d27ed78e97809f3cdeab5a33e3d1826c8
Jim Mayes <jim.mayes@gmail.com>
2015-06-25 16:43:10 -0500

    blank profile for manual sign up users

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,7 @@
 class ProfileController < ApplicationController
     def index
-        @profile = current_user.identities.first.auth_data
+        if current_user.identities
+            @profile = current_user.identities.first.auth_data
+        end
     end
 end
