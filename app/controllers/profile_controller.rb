7e692c8918f67033d2fc661ca4233f0a3e7cd853
Jim Mayes <jim.mayes@gmail.com>
2015-06-25 16:52:11 -0500

    nil profile for non google accounts

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,5 @@
 class ProfileController < ApplicationController
     def index
-        # @profile =| current_user.identities.first.auth_data if current_user.identities
+        @profile = current_user.identities.empty? ? nil : current_user.identities.first.auth_data
     end
 end
