6362ca9891e1281c401336145f426bd196e634b8
Jim Mayes <jim.mayes@gmail.com>
2015-06-25 11:56:51 -0500

    add margin under nav bar

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,5 +1,5 @@
 module ProfileHelper
     def size_avatar(pxl_width, avatar)
-        avatar.slice(0..(str.index('?sz='))) + "?sz=#{pxl_width}"
+        avatar.slice(0..(size_avatar.index('?sz='))) + "?sz=#{pxl_width}"
     end
 end
